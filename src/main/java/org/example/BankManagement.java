4661aa9450708703c5d979c2255722165307d6d5
dragosc1 <dragos.ciobanu232@gmail.com>
2025-03-23 11:03:40 +0200

    Adding basic functions to Bank Management Class

---
+++ src/main/java/org/example/BankManagement.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankManagement {
+    private Map<Integer, Double> bankAccounts;
+
+    public BankManagement() {
+        this.bankAccounts = new HashMap<>();
+    }
+
+    void addBankAccount(int accountId, double amount) {
+        if (bankAccounts.containsKey(accountId)) {
+            System.out.println("Bank account " + Double.toString(amount) + " already exists");
+            return;
+        }
+        this.bankAccounts.put(accountId, amount);
+    }
+
+    void removeBankAccount(int accountId) {
+        if (!bankAccounts.containsKey(accountId)) {
+            System.out.println("Bank account " + Double.toString(accountId) + " does not exist");
+            return;
+        }
+        this.bankAccounts.remove(accountId);
+    }
+
+    void deposit(int accountId, double amount) {
+        if (!bankAccounts.containsKey(accountId)) {
+            System.out.println("Bank account " + Double.toString(amount) + " does not exist");
+            return;
+        }
+        this.bankAccounts.put(accountId, this.bankAccounts.get(accountId) + amount);
+    }
+
+    void withdraw(int accountId, double amount) {
+        if (!bankAccounts.containsKey(accountId)) {
+            System.out.println("Bank account " + Double.toString(amount) + " does not exist");
+            return;
+        }
+        this.bankAccounts.put(accountId, this.bankAccounts.get(accountId) - amount);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
